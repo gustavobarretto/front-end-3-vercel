416e24f85565295a1df72ad5e3d3f6b982b10bd4
gustavo barretto <gustavobarretto@gmail.com>
2021-10-25 19:36:27 -0300

    uploading website test

---
+++ aula-04/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Video from './components/Video';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Video />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ aula-04/src/components/Video/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Video = () => {
+    return (
+        
+        <iframe width="1904" height="768" src="https://www.youtube.com/embed/MoLkabPK3YU?showinfo=0&autoplay=1&mute=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    )
+}
+
+export default Video;
